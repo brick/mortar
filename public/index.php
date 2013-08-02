3e68f0d0d9be1a816daca373af66efcba5725d7c
Benjamin Morel <benjamin.morel@gmail.com>
2013-08-02 18:52:16 +0100

    Added a note about Doctrine EntityManager.

--- public/index.php
+++ public/index.php
@@ -14,21 +14,29 @@ require __DIR__ . '/../vendor/autoload.php';
 spl_autoload_call('Brick\Controller\Annotation\QueryParam');
 spl_autoload_call('Brick\DependencyInjection\Annotation\Inject');
 
-// Create an annotation reader, and an injection policy based on annotations
-$annotationReader = new \Doctrine\Common\Annotations\AnnotationReader();
+// Create an annotation reader, and an injection policy based on annotations.
+$annotationReader = new AnnotationReader();
 $injectionPolicy = new AnnotationPolicy($annotationReader);
 
-// Create and configure the dependency injection container
+// Create and configure the dependency injection container.
 $container = new Container($injectionPolicy);
 $container->bind('Brick\View\ViewRenderer')->toClass('Brick\View\InjectorViewRenderer');
 
-// Create the application
+// Create the application.
 $application = Application::createWithContainer($container);
 
-// Add a route that directly maps a path to a class and method
+// Add a route that directly maps a path to a controller class and method.
 $application->addRoute(new StandardRoute('Mortar\Controller'));
 
-// Plug in the @QueryParam and @PostParam functionality
+/*
+ * Plug in the @QueryParam and @PostParam functionality.
+ *
+ * If you use a Doctrine EntityManager, you can have your entities
+ * autoinstantiated in your controllers by passing a second parameter:
+ *
+ * use Brick\Controller\ParameterConverter\DoctrineConverter;
+ * new RequestParamListener($annotationReader, new DoctrineConverter($entityManager))
+ */
 $application->addEventListener(new RequestParamListener($annotationReader));
 
 $request = Request::getCurrent();
