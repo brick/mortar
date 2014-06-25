a7863f7ffbc580f899173871e6c984cd5ecb8cf4
Benjamin Morel <benjamin.morel@gmail.com>
2014-06-25 20:53:29 +0200

    New method Application::run()

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Brick\Http\Request;
 use Brick\Application\Application;
 use Brick\Routing\Route\StandardRoute;
 
@@ -9,6 +8,4 @@ require __DIR__ . '/../vendor/autoload.php';
 $application = Application::create();
 $application->addRoute(new StandardRoute('Mortar\Controller'));
 
-$request = Request::getCurrent();
-$response = $application->handle($request);
-$response->send();
+$application->run();
